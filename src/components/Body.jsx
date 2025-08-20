800722a4109b3d387c98dfbd1b956c558f61a554
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-20 10:44:24 +0530

    product section

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,8 @@
+import { categories, dummyProducts } from "./utils/constants"
+
 const Body = () => {
   return (
+    <div className="w-full bg-[#1e1e1e] font-[poppins]">
     <section className="w-full h-[100vh] flex items-center justify-center bg-[#1e1e1e]">
         <div className="relative left-10 bg-[#1a1a1a] max-w-80 px-16 py-14 rounded-2xl font-[poppins] text-white z-10">
             <h1 className="text-4xl leading-13 mb-4">Luxury Made Affordable</h1>
@@ -10,6 +13,32 @@ const Body = () => {
             <img className="w-[50rem] h-[40rem] object-cover rounded-2xl" src="https://images.pexels.com/photos/277319/pexels-photo-277319.jpeg?cs=srgb&dl=pexels-pixabay-277319.jpg&fm=jpg" alt="" />
         </div>
     </section>
+    <section className="w-[1050px] mx-auto flex gap-2 justify-center items-center text-center text-white">
+      { categories.map((category) => (
+          <div className="w-1/4 p-5 bg-[#1a1a1a] rounded-xl m-auto">{category}</div>
+      ))}
+    </section>
+    <section className="w-[1050px] mx-auto py-6">
+      
+         <h1 className="text-2xl font-medium tracking-wider py-4 text-white">Trending</h1>
+      <div className="flex justify-center items-center gap-6">
+        {
+          dummyProducts.map((product) => (
+            <div className="w-[15rem] rounded-2xl bg-[#1a1a1a] text-white text-center">
+            <img className="w-full h-[240px] overflow-hidden object-cover object-top rounded-t-2xl" src={product.img} alt="" />
+            <div className="p-4">
+              <h2 className="my-2 font-extralight">{product.heading}</h2>
+              <h3 className="my-2">RS {product.price}/-</h3>
+              <button className="bg-[#057f32] px-4 py-2 rounded-lg">Contact</button>
+              <p className="my-2 text-[12px] text-gray-300">{product.brand}</p>
+            </div>
+        </div>
+          ))
+        }
+      </div>
+      
+    </section>
+    </div>
   )
 }
 
---
+++ src/components/utils/constants.js
@@ -0,0 +1,34 @@
+export const categories = [
+     'Watches', 'Jewelry', 'Audio & Accessories', 'Mobiles & Gadgets', 'Books'
+];
+
+export const dummyProducts = [
+    {
+        img : 'https://images.unsplash.com/photo-1670177257750-9b47927f68eb?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bHV4dXJ5JTIwd2F0Y2h8ZW58MHx8MHx8fDA%3D',
+        heading : 'Tissot watch',
+        price : '999',
+        quality : 'High Quality',
+        brand : 'Tissot'
+    },
+    {
+        img : 'https://res.cloudinary.com/wc-photo/image/upload/c_fill,w_2300,h_1533,g_center/f_auto/q_auto/v1718609598/cms/Top-Luxury-Watch-Brands-Rolex-_7de496abdb?_a=BAVAfVDW0',
+        heading : 'Tissot watch',
+        price : '999',
+        quality : 'High Quality',
+        brand : 'Tissot'
+    },
+    {
+        img : 'https://m.media-amazon.com/images/I/51PoKG1ph2L._UY1000_.jpg',
+        heading : 'Tissot watch',
+        price : '999',
+        quality : 'High Quality',
+        brand : 'Tissot'
+    },
+    {
+        img : 'https://image.made-in-china.com/2f0j00RdMiAJWEMwuL/Men-prime-S-Luxury-Diamond-Custom-Watch-Glass-Ice-Extinguishes-Vvs-Moissanite-Diamond-Watch.webp',
+        heading : 'Tissot watch',
+        price : '999',
+        quality : 'High Quality',
+        brand : 'Tissot'
+    }
+]
